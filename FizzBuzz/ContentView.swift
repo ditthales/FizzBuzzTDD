b37b6fdca95cd979d4f067c3b35ece7025aaeabc
Lucas Migge <53379393+miggelucas@users.noreply.github.com>
2023-02-17 16:52:57 -0300

    livesView done

--- FizzBuzz/ContentView.swift
+++ FizzBuzz/ContentView.swift
@@ -20,13 +20,10 @@ struct ContentView: View {
         ZStack {
             Color(viewModel.colorBackground)
             VStack {
-                HStack {
-                    Spacer()
-                    Text("3 / 5")
-                        .font(.title)
-                        .bold()
-                }
+                LivesView(totalLives: .constant(2), remainingLives: .constant(1))
+                
                 Spacer()
+                
                 VStack (spacing: 25) {
                     HStack (spacing: 25) {
                         Button {
@@ -72,7 +69,9 @@ struct ContentView: View {
                         
                     }
                 }
+                
                 Spacer()
+                
                 HStack {
                     Spacer()
                     Button {
